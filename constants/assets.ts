7789349f895e98703b64ac039a24b258e05ea074
tianlempoy <christianlempoy011@gmail.com>
2026-02-10 20:54:56 +0800

    Add school assets configuration

---
+++ constants/assets.ts
@@ -0,0 +1,17 @@
+export const SCHOOL_ASSETS = {
+    logo: 'path/to/logo.png',
+    banner: 'path/to/banner.png',
+    favicon: 'path/to/favicon.ico'
+};
+
+export const NAV_LINKS = [
+    { name: 'Home', path: '/' },
+    { name: 'About', path: '/about' },
+    { name: 'Contact', path: '/contact' }
+];
+
+export const SCHOOL_INFO = {
+    name: 'SMA Negeri 2 Tompaso',
+    address: 'Jl. Pendidikan No. 1, Tompaso',
+    phone: '(0411) 123456'
+};
